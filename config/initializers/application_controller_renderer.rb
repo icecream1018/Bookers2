8bfc9df8d6f9b7eb4df31aa329f4af82eb1b3b51
icecream1018 <nsmrryt@gmail.com>
2021-08-22 12:16:12 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6fea8bf7b749f24f38f65aaf28443267f5dd987b0aacca150828efc85320eca105a9b262d87be01b7937c09b233d3b11f7bffd6344ba0292cb81bf80664b186f'
